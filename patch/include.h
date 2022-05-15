27652dca8893dbc6556c465581903d24491af758
aCeTotal <lars.oksendal@gmail.com>
2022-05-15 19:05:46 +0200

    dwm

---
+++ patch/include.h
@@ -0,0 +1,30 @@
+/* Bar functionality */
+#include "bar_indicators.h"
+#include "bar_tagicons.h"
+
+#include "bar_alpha.h"
+#include "bar_dwmblocks.h"
+#include "bar_ltsymbol.h"
+#include "bar_status.h"
+#include "bar_status2d.h"
+#include "bar_statuscmd.h"
+#include "bar_tags.h"
+#include "bar_wintitle.h"
+#include "bar_systray.h"
+
+/* Other patches */
+#include "autostart.h"
+#include "fakefullscreenclient.h"
+#include "fullscreen.h"
+#include "shiftviewclients.h"
+#ifdef XINERAMA
+#include "sortscreens.h"
+#endif // XINERAMA
+#include "tagothermonitor.h"
+#include "togglefullscreen.h"
+#include "vanitygaps.h"
+#include "warp.h"
+/* Layouts */
+#include "layout_monocle.h"
+#include "layout_tile.h"
+
